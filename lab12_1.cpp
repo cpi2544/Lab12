eb4ba7cf1d3f5bceb552521ed272a1923188dec9
cpi2544 <pokpong1789@gmail.com>
2023-01-06 15:46:06 +0700

    lab12.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+
+
+
+int main(){
+
+    string grade[] = { "A", "B+", "B", "C+", "C", "D+", "D", "F" ,"W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    srand(time(0));
+    cin.get();
+    cin.get();
+    cin.get();
+    int i = rand()%9;
+
+    cout << "You will get " << grade[i] << " in this 261102.";
+
+    return 0;
+}
+
